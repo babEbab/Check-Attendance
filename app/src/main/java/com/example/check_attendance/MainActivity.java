55e049e2b087ebb4de2198fdb6514447a41762b5
Choi Dasom <dasomchoi52@gmail.com>
2021-11-26 01:07:15 +0900

    Modify activity_main.xml and Add a myDBHelper class in MainActivity.java.

--- app/src/main/java/com/example/check_attendance/MainActivity.java
+++ app/src/main/java/com/example/check_attendance/MainActivity.java
@@ -2,14 +2,45 @@ package com.example.check_attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    myDBHelper myDBHelper;
+    TextView viewSemester;
+    ImageButton addSubjectTop, selectSemester, settings, addSubjectUnder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Check Attendance");
 
+    }
+
+
+    private class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context, "groupDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE subjectTBL(subjectId INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "semester INTEGER, subjectName CHAR(30), subjectProfessor CHAR(20), " +
+                    "subjectMemo CHAR(50), times INTEGER)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS subjectTBL");
+            db.execSQL("DROP TABLE IF EXISTS attInfoTBL"); // Attendance Info Table
+            onCreate(db);
+        }
     }
 }
